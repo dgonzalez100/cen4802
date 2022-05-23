07e452e42794fc30bdc47bfc8b99d15f6f0ad3a7
dgonzalez100 <105597221+dgonzalez100@users.noreply.github.com>
2022-05-22 21:54:40 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,13 +4,10 @@ import java.util.*;
 
 public class Main
 {
-    //method to return the n-th fibonacci sequence number
     public static int fibonacci(int n)
     {
-    //if n is 1 or less then return n
     if (n <= 1)
        return n;
-    //recursively call the function fibonacci()
     return fibonacci(n-1) + fibonacci(n-2);
     }
     //main() method 
@@ -18,12 +15,11 @@ public class Main
     {
     //delare one variable n
     int n;
-    //create Scanner class Object
     Scanner scnr=new Scanner(System.in);
-    //get value of n from user
+
     System.out.println("Enther value of n: ");
     n=scnr.nextInt();
-    //print n-th number in sequence
+        
     System.out.println("The "+n+"th term of the Fibonacci sequence is "+fibonacci(n));
     }
 }
